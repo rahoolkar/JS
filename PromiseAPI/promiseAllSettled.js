e5f011e978f2ff786afda4a0381fed72fd1eb427
rahoolkar <batman30jan2002@gmail.com>
2025-06-08 03:15:40 +0530

    promise.allSettled

---
+++ PromiseAPI/promiseAllSettled.js
@@ -0,0 +1,22 @@
+const p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    //resolve("p1 success");
+    reject("p1 reject");
+  }, 3000);
+});
+
+const p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("p2 success");
+  }, 7000);
+});
+
+const p3 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("p3 success");
+  }, 5000);
+});
+
+Promise.allSettled([p1, p2, p3]).then(function (result) {
+  console.log(result);
+});
